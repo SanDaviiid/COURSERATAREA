4b8096b80068dacb6d66c2f062558b9fea3685aa
David  Medina pineda <W.David.M@macbook-pro-de-david.local>
2016-07-16 19:49:39 -0500

    nueva tarea

---
+++ COURTAREADAV.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//NUEVA TAREA 
+
+for num in 0...100 {
+    if num % 5 == 0 {
+        print ("Bingo")
+    }
+    if num % 2 == 0 {
+        print ("par!!!")
+    }else {
+        print ("impar")
+    }
+    if num >= 30 && num <= 40 {
+        print ("Viva Swift")
+    }
+}
